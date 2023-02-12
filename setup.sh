b6b8e67afc72dbb43f637d10ec671198bb5c2328
Bill Brousalis <bill.brousalis@gmail.com>
2023-02-12 16:27:24 +0200

    install fonts with setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 rm ~/.vimrc ~/.tmux.conf ~/.zshrc
 rsync ~/dotfiles/.* ~/.
+
 mkdir -p ~/.vim/colors
 cp custom.vim ~/.vim/colors/
+
 cp gtk.css ~/.config/gtk-3.0/
+
+mkdir -p ~/.local/share/fonts
+cp fonts/* ~/.local/share/fonts
+fc-cache -vf
